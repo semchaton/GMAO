24ccf6c20c51b5f4a9f1db123b226e6996aaad0d
Victor <victorcharmes.42@gmail.com>
2026-02-11 16:56:46 +0100

    Debut ajout, modif et ajout Machine

--- frontend/src/pages/machine/components/SelectionMachine.jsx
+++ frontend/src/pages/machine/components/SelectionMachine.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react"
 import "../style.css"
+import iconeModificationMachine from "../style/iconeModificationMachine.svg";
+import iconeAjoutMachine from "../style/iconeAjoutMachine.svg";
 
 function SelectionMachine({ machines = [] }) {
   const [selectedMachine, setSelectedMachine] = useState(null)
@@ -14,32 +16,87 @@ function SelectionMachine({ machines = [] }) {
     setSelectedMachine(machine)
   }
 
-  return (
-    <div className="mt-25">
-      <h3 className="ml-15">Nom de la machine:</h3>
+  console.log(machines)
+return (
+  <div className="mt-20">
 
-      <input
-        list="machinesList"
-        className="border-2 rounded border-slate-900 ml-15"
-        placeholder="Choisir ou saisir..."
-        onChange={handleChange}
-      />
+    <img className="ml-10" src={iconeModificationMachine} alt="Modification machine" width="40" />
+    <img className="ml-10 mt-5" src={iconeAjoutMachine} alt="Modification machine" width="40" />
 
-      <datalist id="machinesList">
-        {machines.map(machine => (
-          <option key={machine.id} value={machine.nom} />
-        ))}
-      </datalist>
+    <h3 className="mt-5 ml-15">Nom de la machine:</h3>
 
-      <h3 className="ml-15 mt-10">Criticité :</h3>
+    <input
+      list="machinesList"
+      className="border-2 rounded border-slate-900 ml-15"
+      placeholder="Choisir ou saisir..."
+      onChange={handleChange}
+    />
 
-      <input
-        className="border-2 rounded border-slate-900 ml-15"
-        value={selectedMachine?.criticite || ""}
-        readOnly
-      />
+    <datalist id="machinesList">
+      {machines.map(machine => (
+        <option key={machine.id} value={machine.nom} />
+      ))}
+    </datalist>
+
+    {/* Bloc infos + image */}
+    <div className="flex justify-between items-start mt-10">
+
+      {/* Partie gauche (infos) */}
+      <div className="ml-15 space-y-4">
+
+        <div>
+          <h3>Criticité :</h3>
+          <input
+            className="border-2 rounded border-slate-900"
+            value={selectedMachine?.criticite || ""}
+            readOnly
+          />
+        </div>
+
+        <div>
+          <h3>Classe :</h3>
+          <input
+            className="border-2 rounded border-slate-900"
+            value={selectedMachine?.descriptionClasseOuverture || ""}
+            readOnly
+          />
+        </div>
+
+        <div>
+          <h3>Emplacement :</h3>
+          <input
+            className="border-2 rounded border-slate-900"
+            value={selectedMachine?.emplacement || ""}
+            readOnly
+          />
+        </div>
+
+      </div>
+
+      {/* Partie droite (image) */}
+      {selectedMachine && (
+        <div className="mr-15 border-2 rounded border-slate-900">
+          <img
+            src={`/photosMachines/${selectedMachine.nom}.jpg`}
+            alt={selectedMachine.nom}
+            width="250"
+          />
+        </div>
+      )}
+      <div>
+          <h3 className="mt-0">Description :</h3>
+          <input
+            className="border-2 mr-15 rounded border-slate-900"
+            value={selectedMachine?.description || ""}
+            readOnly
+          />
+      </div>
+
     </div>
-  )
+
+  </div>
+)
+
 }
 
 export default SelectionMachine
